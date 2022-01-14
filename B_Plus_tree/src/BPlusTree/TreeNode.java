a6f412351700ef02ae1e29bf6dc105b80b047b20
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2022-01-14 22:36:08 +0600

    Add files via upload

--- B_Plus_tree/src/BPlusTree/TreeNode.java
+++ B_Plus_tree/src/BPlusTree/TreeNode.java
@@ -3,7 +3,7 @@ package BPlusTree;
 public class TreeNode extends Node{
     private int minSlot;
     private int maxSlot;
-    private int currentKeyCount;
+    private int currentChildCount;
 
     // still unsure about this two
     private TreeNode leftSibling;
@@ -15,7 +15,7 @@ public class TreeNode extends Node{
     public TreeNode (int n, String[] keys) {
         this.maxSlot = n;
         this.minSlot = (int) Math.ceil(n/2.0);
-        this.currentKeyCount = 0;
+        this.currentChildCount = 0;
         this.keys = keys;
         this.childs = new Node[this.maxSlot+1];
     }
@@ -23,14 +23,77 @@ public class TreeNode extends Node{
         this.maxSlot = n;
         this.minSlot = (int) Math.ceil(n/2.0);
         this.keys = keys;
-        this.currentKeyCount = BPlusTree.linierLNullSearch(keys);
+        this.currentChildCount = BPlusTree.linierLNullSearch(keys);
         this.childs = childs;
     }
 
-    public boolean isFull () { return (this.currentKeyCount == this.maxSlot); }
-    public boolean isOverfull () { return (this.currentKeyCount > this.maxSlot); }
-    public boolean isDeficient () { return (this.currentKeyCount < this.minSlot); }
-    public boolean isLendable () { return (this.currentKeyCount > this.minSlot); }
-    public boolean isMergeable () { return (this.currentKeyCount == this.minSlot); }
+    public boolean isFull () { return (this.currentChildCount == this.maxSlot); }
+    public boolean isOverfull () { return (this.currentChildCount > this.maxSlot); }
+    public boolean isDeficient () { return (this.currentChildCount < this.minSlot); }
+    public boolean isLendable () { return (this.currentChildCount > this.minSlot); }
+    public boolean isMergeable () { return (this.currentChildCount == this.minSlot); }
 
+    public void appendChild (Node child) {
+        this.childs[this.currentChildCount] = child;
+        this.currentChildCount++;
+    }
+
+    public void preprendChild (Node child) {
+        for (int i = this.currentChildCount -1; i>=0; i--) { this.childs[i+1] = this.childs[i]; }
+        this.childs[0] = child;
+        this.currentChildCount++;
+    }
+
+    public void inserChild (Node child, int index) {
+        for (int i = this.currentChildCount -1; i>=index; i--) { this.childs[i+1] = this.childs[i]; }
+        this.childs[index] = child;
+        this.currentChildCount++;
+    }
+
+    public int findChildIndex (Node child) {
+        for (int i=0; i<this.childs.length; i++) {
+            if (child == this.childs[i])    return i;
+        }
+        return -1;
+    }
+
+    public void removeKey (int index) {
+        this.keys[index] = null;
+    }
+
+    public void removeChild (int index) {
+        this.childs[index] = null;
+        this.currentChildCount--;
+    }
+
+    public void removeChild (Node child) {
+        for (int i = 0; i<this.currentChildCount; i++) {
+            if (this.childs[i] == child) { this.childs[i] = null; }
+        }
+        this.currentChildCount--;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
